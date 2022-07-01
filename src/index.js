4bb9ddcd48773d52e6895c8ec4871e1460cbaa9f
AndriiSkrypka <andriskrupka25011994@icloud.com>
2022-07-01 11:59:56 +0300

    dz-11-js

---
+++ src/index.js
@@ -0,0 +1,95 @@
+import Notiflix from 'notiflix';
+import fetchImg from './js/api';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const form = document.querySelector('.search-form');
+const wrapGallery = document.querySelector('.gallery');
+const target = document.querySelector('.target');
+
+const options = {
+  root: null,
+  rootMargin: '400px',
+  threshold: 1.0,
+};
+
+const observe = new IntersectionObserver(generateImg, options);
+
+form.addEventListener('submit', submit);
+
+let page = 1;
+let onInput = '';
+
+function submit(event) {
+  event.preventDefault();
+
+  onInput = event.target.searchQuery.value;
+
+  wrapGallery.innerHTML = '';
+  observe.unobserve(target);
+
+  if (!onInput) {
+    Notiflix.Notify.failure('Please, search any picture!');
+    return;
+  }
+  fetchImg(onInput, page).then(response => {
+    if (!response.data.total) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    } else {
+      createMarkup(response.data.hits);
+      observe.observe(target);
+    }
+    Notiflix.Notify.success(`Hooray! We found ${response.data.total} images.`);
+  });
+}
+
+function createMarkup(array) {
+  const imgList = array
+    .map(item => {
+      return `
+    <div class="photo-card-gallary"> 
+      <a href="${item.largeImageURL}">
+        <img  src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+      </a>
+      <div class="info-gallary">
+        <p class="info-item-gallary">
+          <b>Likes: ${item.likes}</b>
+        </p>
+        <p class="info-item-gallary">
+          <b>Views: ${item.views}</b>
+        </p>
+        <p class="info-item-gallary">
+          <b>Comments: ${item.comments}</b>
+        </p>
+        <p class="info-item-gallary">
+          <b>Downloads: ${item.downloads}</b>
+        </p>
+      </div>
+    </div>`;
+    })
+    .join('');
+
+  wrapGallery.insertAdjacentHTML('beforeend', imgList);
+
+  const simpleBox = new SimpleLightbox('.gallery a', {});
+}
+
+function generateImg(entries) {
+  entries.forEach(entrie => {
+    if (entrie.isIntersecting) {
+      page += 1;
+      fetchImg(onInput, page).then(response => {
+        if (response.data.hits.length === 0) {
+          Notiflix.Notify.failure(
+            `We're sorry, but you've reached the end of search results.`
+          );
+          return;
+        }
+        createMarkup(response.data.hits);
+      });
+    }
+  });
+}
---
+++ src/js/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export default async function fetchImg(value, page) {
+  try {
+    const response = await axios({
+      url: `https://pixabay.com/api/`,
+      params: {
+        key: '28349492-852ddd1eacec879b1ac2fb90f',
+        q: value,
+        orientation: 'horizontal',
+        image_type: 'photo',
+        safesearch: true,
+        page: page,
+        per_page: 40,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
